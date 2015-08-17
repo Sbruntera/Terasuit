bec8328fa381458d766ec73fb0f43ec5af72c270
Jan Rogge <j-p_rogge@live.de>
2015-08-17 16:01:52 +0200

    Simply test

---
+++ Terasuit/src/logic/Login_formula_test.java
@@ -0,0 +1,13 @@
+package logic;
+
+public class Login_formula_test {
+
+	public Login_formula_test(){
+		System.out.println("Dat wetter ist so schlimm");
+		System.out.println("Drecks kopfschmerzen");
+		/*
+		 * Login Forumla -> Daten werden gesendet -> Server überprüft Daten -> Server sendet Antwort -> User ist eingeloggt
+		 * 
+		 */
+	}
+}
