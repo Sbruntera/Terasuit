9879ff54ac50ac6388698cd51be695bd0ca7a51d
Sbruntera <schraubeqq@hotmail.com>
2015-08-21 14:45:16 +0200

    .

--- Terasuit/src/grafig/StartPanel.java
+++ Terasuit/src/grafig/StartPanel.java
@@ -5,7 +5,8 @@ import java.awt.Dimension;
 import javax.swing.JPanel;
 
 public class StartPanel extends JPanel{
-	
+
+	private static final long serialVersionUID = 1L;
 	public static int HEIGHT = 300;
 	public static int WIGTH = 450;
 	
