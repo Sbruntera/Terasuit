f8f076d15c2b9502e862b663f4b4cc167ab6e316
Jan Rogge <j-p_rogge@live.de>
2015-12-29 19:35:18 +0100

    Changed DB.java - now includes Stat Table and Method to add a Number to a Stat

--- Terasuit/src/logic/START.java
+++ Terasuit/src/logic/START.java
@@ -6,8 +6,8 @@ public class START {
 	
 	public static void main(String[] args) {
 		
-		//Client_test t = new Client_test("register,Admin,use,test@mail.com,Admin");
-		Client_test a = new Client_test("login,admin,pw");
+		Client_test t = new Client_test("register,Admin,use,test@mail.com,Admin");
+		//Client_test a = new Client_test("login,admin,pw");
 		
 		
 	}
